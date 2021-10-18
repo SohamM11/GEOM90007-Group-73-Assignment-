fad381a791b004d4f1777a74bbb37facf3735484
sohamdighe14 <sohamdighe14@gmail.com>
2021-10-18 19:04:11 +0530

    red line might be only till track ends. See this on wed.

--- JS/exec.js
+++ JS/exec.js
@@ -104,3 +104,61 @@ const map3 = new mapboxgl.Map({
 });
 map3.addControl(new mapboxgl.NavigationControl());
 map3.addControl(new mapboxgl.ScaleControl({position: 'bottom-right'}));
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2RpZ2hlIiwiYSI6ImNrdDN3OTdpajB5Y2Yyb3BnOTM1bG9xNnAifQ._3qrPrtiR2sRnuaGWG6tZA'; 
+const map4 = new mapboxgl.Map({
+    container: 'map4', // container ID
+    style: 'mapbox://styles/sdighe/ckupuacdehsq818s0g9f0bg1r', // style URL  
+});
+var popup = new mapboxgl.Popup({
+    closeOnClick: false
+})
+map4.on('mouseenter', 'ptv-metro-tram-stops-point-9zgd4m', (e) => {
+    map4.getCanvas().style.cursor = 'pointer';
+    popup
+    .setLngLat(e.lngLat)
+    .setHTML(e.features[0].properties.STOP_NAME)
+    .addTo(map4);
+    });
+map4.on('mouseleave', 'ptv-metro-tram-stops-point-9zgd4m', () => {
+    map4.getCanvas().style.cursor = '';
+    popup.remove();
+    })
+
+map4.on('click', 'ptv-metro-tram-routes-line-axyn70', (e) => {
+    new mapboxgl.Popup()
+    .setLngLat(e.lngLat)
+    .setHTML('<div style="width: 3000px"><table><tbody><tr><td>Starting Station:</td></tr><tr><td>'+e.features[0].properties.FIRST_STO8+'</td></tr><tr><td>Ending Station:</td></tr><tr><td>'+e.features[0].properties.LAST_STOP2+'</td></tr><tr><td>Length of route:</td></tr><tr><td>'+ Math.round(e.features[0].properties.ROUTE_KM)+' Km</td></tr></tbody></table></div>')
+    .addTo(map4);
+    if (typeof map4.getLayer('selectedRoad') !== "undefined" ){         
+        map4.removeLayer('selectedRoad')
+        map4.removeSource('selectedRoad');   
+    }
+    var features = map4.queryRenderedFeatures(e.point, { layers: ['ptv-metro-tram-routes-line-axyn70'] });
+    var feature = features[0];
+    map4.addSource('selectedRoad', {
+        "type":"geojson",
+        "data": feature.toJSON()
+    });
+    map4.addLayer({
+        "id": "selectedRoad",
+        "type": "line",
+        "source": "selectedRoad",
+        "layout": {
+            "line-join": "round",
+            "line-cap": "round"
+        },
+        "paint": {
+            "line-color": "red",
+            "line-width": 4
+        }
+    });
+    });
+map4.on('mouseenter', 'ptv-metro-tram-routes-line-axyn70', (e) => {
+    map4.getCanvas().style.cursor = 'pointer';
+    });
+map4.on('mouseleave', 'ptv-metro-tram-routes-line-axyn70', () => {
+    map4.getCanvas().style.cursor = '';
+    });
+map4.addControl(new mapboxgl.NavigationControl());
+map4.addControl(new mapboxgl.ScaleControl({position: 'bottom-right'}));
